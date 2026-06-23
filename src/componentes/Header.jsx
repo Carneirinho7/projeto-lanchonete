4c035a378410943f1c2f555699f5bfb542d4f5e4
unknown <thiagocarneiro2525@gmail.com>
2026-06-23 17:15:58 -0300

    att2

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,8 +1,18 @@
+import'./Header.css'
+
+
+<Header className="css"></Header>
+
+
 function Header({titulo,subtitulo}){
     return(
  <> 
+        <div className="titulo">
+
         <h1>{titulo}</h1>
         <h2>{subtitulo}</h2>
+
+        </div>
  </>
     )
    
