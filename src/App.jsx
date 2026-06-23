6aaa398a7f6ec9310b1d016fa154377c71e652da
unknown <thiagocarneiro2525@gmail.com>
2026-06-23 16:31:24 -0300

    att2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./componentes/Header";
+import Login from "./componentes/Login";
+
+
+function App(){
+  return(
+    <>
+      <Header titulo = "Lanchonete Dogao"
+      subtitulo = "A melhor de CWB" />
+      <Login  />
+    </>
+
+  )
+}
+export default App
